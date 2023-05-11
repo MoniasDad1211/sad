54b532ac6273f235b2b969647d9141ca6196b9f2
MoniasDad1211 <133230133+MoniasDad1211@users.noreply.github.com>
2023-05-11 18:04:43 +0500

    Create Tags.lua

---
+++ NTDCore/Chattags/main/Tags.lua
@@ -0,0 +1,74 @@
+return {
+	["AstralVaspe"] = {
+		TagText = "NIGHTBED OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["PrismUserz"] = {
+		TagText = "PRISM USER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["forgornameboblus"] = {
+		TagText = "YZFloppa",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["IIIIIIIIIIIIiIIi1441"] = {
+		TagText = "NIGHTBED OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["xAom95"] = {
+		TagText = "NIGHTBED OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["MoniaReturn"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["HellBladeRelease"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["sadasa"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["JosepthSStalin"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["qwertyui_bigmac"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["table0x768"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["XylexIsCanadain"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["table0x472"] = {
+		TagText = "RISE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["MaxlaserTechOnTop6"] = {
+		TagText = "ZEPHYR/LAZERWARE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+        ["IIIIIIIIIIIIiIIi1441"] = {
+		TagText = "ZEPHYR PRIVATE",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["JustAnAverageFloppa2"] = {
+		TagText = "Ultra Racism",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["IIIIIIIIIIIIiIIi1441"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["MessiMessiREAL"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	}
+}
